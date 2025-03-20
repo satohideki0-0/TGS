fe12be6cf1fa1001cdf38db7cc5b143f6c3e39f5
DESKTOP-EPJMN7P\sato <satoone20@gmail.com>
2025-03-21 01:03:17 +0900

    コミット3

--- Assets/satoNoZikkenjo/TalkData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TalkData", menuName = "Scriptable Objects/TalkData")]
-public class TalkData : ScriptableObject
-{
-    [TextArea(3, 5)]
-    public string[] messages; // 会話の文章（複数行対応）  
-}
